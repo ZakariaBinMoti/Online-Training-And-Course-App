93181d2b35611cfe8a01ca35a590acd820eb7442
Protick Saha <85408091+Protick10@users.noreply.github.com>
2023-12-26 01:46:07 +0600

    some feature added

--- app/src/main/java/com/example/onlinetrainingandcourse/AllTopics.java
+++ app/src/main/java/com/example/onlinetrainingandcourse/AllTopics.java
@@ -1,14 +1,33 @@
 package com.example.onlinetrainingandcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+
 public class AllTopics extends AppCompatActivity {
     private ImageView homefooterpannel, homeHomebtn, homeedubtn, homesrcbtn, homesavebtn, homeprofilebtn;
+    RecyclerView recyclerView;
+
+
+    private AllTopicAdapter adapter;
+    private List<AllTopicModel> topicList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +38,13 @@ public class AllTopics extends AppCompatActivity {
         homesrcbtn = findViewById(R.id.homesrcbtn);
         homesavebtn = findViewById(R.id.homesavebtn);
         homeprofilebtn = findViewById(R.id.homeprofilebtn);
+        recyclerView = findViewById(R.id.topic_list);
+
+        // Create a LinearLayoutManager with horizontal orientation
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        // Set the LayoutManager for the RecyclerView
+        recyclerView.setLayoutManager(layoutManager);
 
         homeHomebtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +77,40 @@ public class AllTopics extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+//        topicList = new ArrayList<>();
+//        adapter = new AllTopicAdapter(topicList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setAdapter(adapter);
+//
+//        // Fetch data from Firebase Realtime Database
+//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("alltopic");
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                topicList.clear();
+//                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+//                    AllTopicModel topic = dataSnapshot.getValue(AllTopicModel.class);
+//                    topicList.add(topic);
+//                }
+//                adapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                // Handle errors
+//            }
+//        });
+
+        FirebaseRecyclerOptions<AllTopicModel> options =
+                new FirebaseRecyclerOptions.Builder<AllTopicModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("alltopic"), AllTopicModel.class)
+                        .build();
+
+        adapter = new AllTopicAdapter(options);
+        recyclerView.setAdapter(adapter);
+
+
 
     }
 }
--- app/src/main/java/com/example/onlinetrainingandcourse/EducationActivity.java
+++ app/src/main/java/com/example/onlinetrainingandcourse/EducationActivity.java
@@ -1,14 +1,77 @@
 package com.example.onlinetrainingandcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SearchView;
+//import android.widget.Toolbar;
+
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.FirebaseDatabase;
+import androidx.appcompat.widget.Toolbar;
 
 public class EducationActivity extends AppCompatActivity {
+
+    private Toolbar careerToolbar;
+    private SearchView careerSearchView;
+    private SwipeRefreshLayout careerRefreshLayout;
+    private RecyclerView careerListView;
+    private FloatingActionButton careerFilterFab;
+    HomeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education);
+
+        // Initialize variables using findViewById
+        careerToolbar = findViewById(R.id.career_toolbar);
+        careerSearchView = findViewById(R.id.career_search_view);
+//        careerRefreshLayout = findViewById(R.id.career_refresh_layout);
+        careerListView = findViewById(R.id.career_list_view);
+        careerFilterFab = findViewById(R.id.career_filter_fab);
+
+        // Set up your RecyclerView (example: setLayoutManager)
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        careerListView.setLayoutManager(layoutManager);
+
+        // Now you can use these variables as needed in your activity
+        // For example, you can set up a click listener for the FloatingActionButton
+        careerFilterFab.setOnClickListener(view -> {
+            // Handle the click event for the FloatingActionButton
+        });
+
+        FirebaseRecyclerOptions<HomeModel> options =
+                new FirebaseRecyclerOptions.Builder<HomeModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("homecard"), HomeModel.class)
+                        .build();
+
+
+        adapter = new HomeAdapter(options, new HomeAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(HomeModel model) {
+                // Handle item click here, e.g., start CodeDetailsActivity and pass data
+                Intent intent = new Intent(EducationActivity.this, CodeDetailsActivity.class);
+                intent.putExtra("coursename", model.getCoursename());
+                intent.putExtra("description", model.getDescription());
+                intent.putExtra("offerby", model.getOfferby());
+                intent.putExtra("videourl", model.getVideourl());
+                intent.putExtra("rating", model.getRating());
+                // Add any other data you want to pass to CodeDetailsActivity
+                startActivity(intent);
+            }
+        });
+        careerListView.setAdapter(adapter);
+
+
     }
+
+
+
 }
